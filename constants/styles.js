187f9caddb2675ae2668415aa8feecd2f8e0ee7c
Shreeja Das <88001308+shreejalearn@users.noreply.github.com>
2023-08-25 16:56:47 -0400

    Add files via upload

---
+++ constants/styles.js
@@ -0,0 +1,8 @@
+export const Colors = {
+  primary100: '#f8e3e3',
+  primary500: '#7D5A5A',
+  primary800: '#966e6e',
+  primary900: '#F1D1D1',
+  error100: '#F3E1E1',
+  error500: '#ffb8b8',
+}
---
+++ util/auth.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const API_KEY = 'Your API Key Here';
+async function authenticate(mode, email, password) {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${KEY}`;
+
+  const response = await axios.post(url, {
+    email: email,
+    password: password,
+    returnSecureToken: true,
+  });
+
+  const token = response.data.idToken;
+
+  return token;
+}
+
+export function createUser(email, password) {
+  return authenticate('signUp', email, password);
+}
+
+export function login(email, password) {
+  return authenticate('signInWithPassword', email, password);
+}
